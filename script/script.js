9bca9594a290c66c0c4ee068c0ccc59c597066df
Thiago Mesquita <83050372+ThiagoM3sk@users.noreply.github.com>
2021-10-22 20:54:58 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+/*
+let sim = window.prompt('Concordas que o Eye chupa pica?'z)
+if(sim != 'sim'){
+    window.alert('Então quem chupa pica é tu')
+}else{
+    window.alert('Obrigado')
+}
+*/
+function sure(){
+    window.alert('Tu chupa muito é pica')
+}
+/*if (window.confirm("Você realmente quer sair?")) {
+    window.open("sair.html", "Obrigado pela visita!");
+  }*/
