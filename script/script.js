72c9545642c003026f8162b594d5c6e408c424fc
Thiago Mesquita <thyagomesk@gmail.com>
2022-04-09 15:51:49 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -84,20 +84,5 @@ switch(diaSem){
         break
 }
 
-
 tempo.innerHTML = `Visitado em ${diaSem}, ${dia}/${mes}/${ano}`
 */
-/*
-let sim = window.prompt('Concordas que o Eye chupa pica?'z)
-if(sim != 'sim'){
-    window.alert('Então quem chupa pica é tu')
-}else{
-    window.alert('Obrigado')
-}
-
-function sure(){
-    window.alert('Tu ta querendo muito é pica e não ta sabendo pedir')
-}
-if (window.confirm("Você realmente quer sair?")) {
-    window.open("sair.html", "Obrigado pela visita!");
-  }*/
